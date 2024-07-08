5a8ef378a1e9b2e48fb161f8b0c3a76f1bc532a6
Harsh <harshbaliportfolio@gmail.com>
2024-07-08 21:42:13 +0100

    Updated script

--- payroll-farming-script/payroll.py
+++ payroll-farming-script/payroll.py
@@ -14,7 +14,7 @@ ws = wb.active
 # Add headers to the worksheet
 headers =['First-Name','Last-Name',
         'Date-of-birth','Joining-Date', 
-        'Address', 'Position', 'Salary', 
+        'Address', 'Department', 'Salary', 
         'Gender', 'NI number', 'Right-to-work' ] 
 
 ws.append(headers)
@@ -28,8 +28,10 @@ for _ in range(1000):
         fake.address(),
         fake.date_of_birth(),
         fake.date_this_decade(),
-        fake.job(),
+        # Create departments 
+        fake.random_element(elements=('HR', 'Artist', 'Producer', 'Promoter', 'Technician', 'Editor')),
         fake.random_number(digits=5),
+        # Create Genders
         fake.random_element(elements=('Male', 'Female')),
         fake.random_number(digits=9),
         fake.boolean()
